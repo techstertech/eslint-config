dcfbcc351ceab1bd14c7607ecb761f5cc6e7edfd
Leonardo Venturini <leovenbag@gmail.com>
2022-09-19 13:21:55 -0400

    add sass support to eslint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   "extends": [
     "stylelint-config-standard",
     "stylelint-config-prettier",
-    "stylelint-config-tailwindcss"
+    "stylelint-config-tailwindcss",
+    "stylelint-config-standard-scss"
   ],
   "rules": {
     "at-rule-no-unknown": null
