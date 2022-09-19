2b663a07dc25e5aab4c50a56ad6b8dbd8abeeb62
Leonardo Venturini <leovenbag@gmail.com>
2022-09-19 13:32:08 -0400

    fix quotes

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-  "extends": [
-    "stylelint-config-standard",
-    "stylelint-config-prettier",
-    "stylelint-config-tailwindcss",
-    "stylelint-config-standard-scss"
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier',
+    'stylelint-config-tailwindcss',
+    'stylelint-config-standard-scss',
   ],
-  "rules": {
-    "at-rule-no-unknown": null,
-    "string-quotes": "single",
-  }
+  rules: {
+    'at-rule-no-unknown': null,
+    'string-quotes': 'single',
+  },
 }
